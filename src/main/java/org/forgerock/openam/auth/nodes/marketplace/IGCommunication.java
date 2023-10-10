6cada4c4b025ca3d13d353a3151c661d30820778
Justin Chin <55926235+justinchinFR@users.noreply.github.com>
2023-10-10 10:27:54 -0400

    Update IGCommunication.java
    Updated Flow - sign then encrypt

--- src/main/java/org/forgerock/openam/auth/nodes/marketplace/IGCommunication.java
+++ src/main/java/org/forgerock/openam/auth/nodes/marketplace/IGCommunication.java
@@ -7,6 +7,7 @@
 package org.forgerock.openam.auth.nodes.marketplace;
 
 import java.security.SecureRandom;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.time.Clock;
 import java.time.Instant;
@@ -20,17 +21,19 @@ import java.util.UUID;
 import javax.inject.Inject;
 
 import org.forgerock.json.JsonValue;
-import org.forgerock.json.jose.builders.EncryptedThenSignedJwtBuilder;
 import org.forgerock.json.jose.builders.JwtBuilderFactory;
 import org.forgerock.json.jose.builders.SignedJwtBuilderImpl;
+import org.forgerock.json.jose.exceptions.JweDecryptionCheckedException;
+import org.forgerock.json.jose.jwe.EncryptedJwt;
 import org.forgerock.json.jose.jwe.EncryptionMethod;
 import org.forgerock.json.jose.jwe.JweAlgorithm;
+import org.forgerock.json.jose.jwe.JweHeader;
+import org.forgerock.json.jose.jwe.SignedThenEncryptedJwt;
 import org.forgerock.json.jose.jwk.RsaJWK;
-import org.forgerock.json.jose.jws.EncryptedThenSignedJwt;
 import org.forgerock.json.jose.jws.JwsAlgorithm;
-import org.forgerock.json.jose.jws.SigningManager;
+import org.forgerock.json.jose.jws.JwsHeader;
+import org.forgerock.json.jose.jws.SignedJwt;
 import org.forgerock.json.jose.jws.handlers.SecretRSASigningHandler;
-import org.forgerock.json.jose.jws.handlers.SigningHandler;
 import org.forgerock.json.jose.jwt.JwtClaimsSet;
 import org.forgerock.openam.annotations.sm.Attribute;
 import org.forgerock.openam.auth.node.api.AbstractDecisionNode;
@@ -40,11 +43,14 @@ import org.forgerock.openam.auth.node.api.NodeState;
 import org.forgerock.openam.auth.node.api.TreeContext;
 import org.forgerock.openam.core.realms.Realm;
 import org.forgerock.openam.sm.AnnotatedServiceRegistry;
+import org.forgerock.secrets.Purpose;
 import org.forgerock.secrets.SecretBuilder;
 import org.forgerock.secrets.SecretsProvider;
-import org.forgerock.secrets.keys.KeyEncryptionKey;
+import org.forgerock.secrets.keys.KeyDecryptionKey;
 import org.forgerock.secrets.keys.SigningKey;
+import org.forgerock.secrets.keys.VerificationKey;
 import org.forgerock.util.i18n.PreferredLocales;
+import org.forgerock.util.promise.Promise;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -120,7 +126,7 @@ public class IGCommunication extends AbstractDecisionNode {
 			//does the JWT exist in the returning request parameters
 			if (context.request.parameters.get(igCommConfig.returningJWTName())!=null) {
 				//it does... lets get to work... check signature, decrypt and map to sharedstate
-				JwtClaimsSet theClaimSet = verifyAndDecryptFromIG(context, context.request.parameters.get(igCommConfig.returningJWTName()).get(0));
+				JwtClaimsSet theClaimSet = decryptThenVerifyFromIG(context, context.request.parameters.get(igCommConfig.returningJWTName()).get(0));
 				mapClaimsToSS(context, theClaimSet);
 				return Action.goTo(SUCCESS).build();
 			}
@@ -132,7 +138,7 @@ public class IGCommunication extends AbstractDecisionNode {
 					sendString = getSignedJWT(context);
 					break;
 				case "EncryptAndSign":
-					sendString = getEncryptedThenSignedJWT(context);
+					sendString = getSignThenEncryptJWT(context);
 				}
 				
 				//redirect the user
@@ -153,31 +159,6 @@ public class IGCommunication extends AbstractDecisionNode {
 		}
 	}
 	
-	private JwtClaimsSet verifyAndDecryptFromIG(TreeContext context, String jwt) throws Exception {
-		String nonce = context.getStateFor(this).get(NONCE).asString();
-		context.getStateFor(this).remove(NONCE);
-		
-		JwtBuilderFactory jwtBuilderFactory = new JwtBuilderFactory();
-		EncryptedThenSignedJwt readMe = jwtBuilderFactory.reconstruct(jwt, EncryptedThenSignedJwt.class);
-		RsaJWK publicRSAJWK = RsaJWK.parse(this.igCommConfig.signPublicKey());
-		SigningHandler verificationHandler = new SigningManager(new SecretsProvider(Clock.systemDefaultZone())).newVerificationHandler(publicRSAJWK);
-		boolean valid = readMe.verify(verificationHandler);
-		
-		if (!valid) {
-			throw new Exception("Signature check fail for returning JWT from Gateway. Here is the JWT: " + jwt);
-		}
-
-		RsaJWK privateRSAJWK = RsaJWK.parse(this.igCommConfig.decryptPrivateKey());	
-		//TODO need to figure out to use the supported decrypt
-		readMe.decrypt(privateRSAJWK.toRSAPrivateKey());
-		JwtClaimsSet jcs = readMe.getClaimsSet();
-		Date now = new Date();
-		if (jcs.getExpirationTime().after(now) && jcs.getNotBeforeTime().before(now) && jcs.getSubject().equalsIgnoreCase(nonce))
-			return jcs;
-		else 
-			throw new Exception("After sign check pass and decrypt of JWT coming back from IG, the expiration date was off, or the not before date failed or the nonce did not match what was stored in this session.  Heres the nonce: " + nonce + ".  Here are the claims returned: " + jcs.build());
-	}
-	
 	private RedirectCallback getRDCallback(TreeContext context, String sendString) throws Exception{
 		String redirectUrl = this.igCommConfig.igURL() + config.route() + "/?" + igCommConfig.returningJWTName() + "=" + sendString;
 		RedirectCallback redirect = new RedirectCallback(redirectUrl, null, "GET");
@@ -185,45 +166,79 @@ public class IGCommunication extends AbstractDecisionNode {
 		return redirect;
 	}
 	
-	private String getEncryptedThenSignedJWT(TreeContext context) throws Exception{
+	private String getSignThenEncryptJWT(TreeContext context) throws Exception{
 		SecureRandom sr = new SecureRandom();
 		long randomLong = Math.abs(sr.nextLong());
 		
 		RsaJWK privateRSAJWK = RsaJWK.parse(this.igCommConfig.decryptPrivateKey());		
 		RsaJWK publicRSAJWK = RsaJWK.parse(this.igCommConfig.signPublicKey());
 		
-		// sign setup
-		SecretBuilder ssb = new SecretBuilder();
-		ssb.expiresAt(Instant.now().plusMillis(this.igCommConfig.jwtExpiration()));
-		ssb.stableId(Long.toString(randomLong));
-		ssb.secretKey(privateRSAJWK.toRSAPrivateKey());
-		SigningKey sk = new SigningKey(ssb);
-		SecretRSASigningHandler srsh = new SecretRSASigningHandler(sk);
-
+		RSAPublicKey publicRsaKey = publicRSAJWK.toRSAPublicKey();
+		RSAPrivateKey privateRsaKey = privateRSAJWK.toRSAPrivateKey();
 		
-		//Encryption setup
+		JwsHeader privateJWSHeader = new JwsHeader();
+		privateJWSHeader.setAlgorithm(JwsAlgorithm.RS256);
+
 		SecretBuilder sb = new SecretBuilder();
+		sb.secretKey(privateRsaKey);
 		sb.expiresAt(Instant.now().plusMillis(this.igCommConfig.jwtExpiration()));
 		sb.stableId(Long.toString(randomLong));
-		RSAPublicKey rpk = publicRSAJWK.toRSAPublicKey();
-		sb.publicKey(rpk);
-		KeyEncryptionKey kek = new KeyEncryptionKey(sb);
 
-		JweAlgorithm jweAlgorithm = JweAlgorithm.RSA_OAEP_256;
-		EncryptionMethod encryptionMethod = EncryptionMethod.A256GCM;
+		SigningKey sk = new SigningKey(sb);
 
-		JwtBuilderFactory jwtBuilderFactory = new JwtBuilderFactory();
+		// Create JWT
+		SignedJwt signedJWT = new SignedJwt(privateJWSHeader, getClaimSetToIG(context, getSetNonce(context)), new SecretRSASigningHandler(sk));
+
+		JweHeader publicJWEHeader = new JweHeader();
+		publicJWEHeader.setAlgorithm(JweAlgorithm.RSA_OAEP_256);
+		publicJWEHeader.setEncryptionMethod(EncryptionMethod.A256GCM);
+
+		SignedThenEncryptedJwt stej = new SignedThenEncryptedJwt(publicJWEHeader, signedJWT, publicRsaKey);
+
+		return stej.build();
+	}
+	
+	private JwtClaimsSet decryptThenVerifyFromIG(TreeContext context, String jwt) throws Exception {
+		String nonce = context.getStateFor(this).get(NONCE).asString();
+		context.getStateFor(this).remove(NONCE);
 		
-		EncryptedThenSignedJwtBuilder jwtBuilder = jwtBuilderFactory
-				.jwe(kek)
-				.headers()
-				.alg(jweAlgorithm)
-				.enc(encryptionMethod).done()
-				.claims(getClaimSetToIG(context, getSetNonce(context)))
-				.signedWith(srsh, JwsAlgorithm.RS256);
+		JwtBuilderFactory jwtBuilderFactory = new JwtBuilderFactory();
+		SignedThenEncryptedJwt readMe = new SignedThenEncryptedJwt(jwtBuilderFactory.reconstruct(jwt, EncryptedJwt.class));
+		RsaJWK publicRSAJWK = RsaJWK.parse(this.igCommConfig.signPublicKey());
+		RsaJWK privateRSAJWK = RsaJWK.parse(this.igCommConfig.decryptPrivateKey());		
 
-		String ets = jwtBuilder.build();
-		return ets;
+		
+		Clock thisClock = Clock.systemUTC();
+		Date expireDate = new Date(new Date().getTime() + this.igCommConfig.jwtExpiration());
+		
+		RSAPublicKey publicRsaKey = publicRSAJWK.toRSAPublicKey();
+		RSAPrivateKey privateRsaKey = privateRSAJWK.toRSAPrivateKey();
+		
+		
+		SecretsProvider sp = new SecretsProvider(thisClock);
+		SecretBuilder sbPubK = new SecretBuilder();
+		sbPubK.publicKey(publicRsaKey);
+		sbPubK.expiresAt(expireDate.toInstant());
+		Purpose<VerificationKey> vPurpose = Purpose.purpose("SignCheck", VerificationKey.class);
+		sp.useSpecificSecretForPurpose(vPurpose, sbPubK.build(vPurpose));
+
+		
+		SecretBuilder sbPriK = new SecretBuilder();
+		sbPriK.secretKey(privateRsaKey);
+		sbPriK.expiresAt(expireDate.toInstant());
+		Purpose<KeyDecryptionKey> dPurpose = Purpose.purpose("DecryptCheck", KeyDecryptionKey.class);
+		sp.useSpecificSecretForPurpose(dPurpose, sbPriK.build(dPurpose));
+
+		
+		Promise<SignedThenEncryptedJwt, JweDecryptionCheckedException> retObj = readMe.decryptAndVerify(sp, dPurpose, vPurpose);
+		SignedThenEncryptedJwt thisResult = retObj.getOrThrow();
+		
+		JwtClaimsSet jcs = thisResult.getClaimsSet();
+		Date now = new Date();
+		if (jcs.getExpirationTime().after(now) && jcs.getNotBeforeTime().before(now) && jcs.getSubject().equalsIgnoreCase(nonce))
+			return jcs;
+		else 
+			throw new Exception("After sign check pass and decrypt of JWT coming back from IG, the expiration date was off, or the not before date failed or the nonce did not match what was stored in this session.  Heres the nonce: " + nonce + ".  Here are the claims returned: " + jcs.build());		
 	}
 	
 	private String getSignedJWT(TreeContext context) throws Exception{
